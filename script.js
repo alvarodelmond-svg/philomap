d70c3686cadd5195ff384f3d46c984b7f05fc4f9
Alvaro <alvaro.delmond@estudante.ifto.edu>
2026-03-10 17:39:17 -0300

    nova atividade

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("JS funcionando");
+const botaoRecomendacao = document.getElementById("botaoRecomendacao");
+const areaRecomendacao = document.getElementById("recomendacao");
+
+const recomendacoes = [
+{
+tipo: "Livro",
+titulo: "O Estrangeiro",
+autor: "Albert Camus"
+},
+
+{
+tipo: "Filme",
+titulo: "Matrix",
+autor: "Irmãs Wachowski"
+},
+
+{
+tipo: "Livro",
+titulo: "Apologia de Sócrates",
+autor: "Platão"
+},
+
+{
+tipo: "Podcast",
+titulo: "Filosofia Pop",
+autor: "Episódio sobre Existencialismo"
+},
+
+{
+tipo: "Filme",
+titulo: "Clube da Luta",
+autor: "David Fincher"
+}
+];
+
+botaoRecomendacao.addEventListener("click", mostrarRecomendacao);
+
+function mostrarRecomendacao(){
+
+const indiceAleatorio = Math.floor(Math.random() * recomendacoes.length);
+
+const item = recomendacoes[indiceAleatorio];
+
+areaRecomendacao.innerHTML =
+`<strong>${item.tipo}</strong>: ${item.titulo} <br> ${item.autor}`;
+
+}
