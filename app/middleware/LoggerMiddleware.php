10499dada4b051d38e3be82500b229994cec919a
Alvaro <alvaro.delmond@estudante.ifto.edu>
2026-05-19 18:02:53 -0300

    retrocesso

--- app/middleware/LoggerMiddleware.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class LoggerMiddleware {
-    public function handle() {
-        // Exemplo: Registrar no log de acesso
-        // error_log("Acessando página de matrícula em " . date('Y-m-d H:i:s'));
-        return true; // Se retornar false, a rota é interrompida
-    }
-}
